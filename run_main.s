286ee5fd5c8361332e443afcd8a921a2cf903ad3
Dan Marom <danmarom16@outlook.com>
2021-12-07 23:09:58 +0200

    Ex3: Mission Completed - Version 2.0

--- run_main.s
+++ run_main.s
@@ -27,41 +27,41 @@
         movq %rsp, %rbp
 
         ## scan 1st argument p1.len
-        subq $528, %rsp             # allocate 2*216 for Pstrings, 2*4 for integers, 8 for allignment
+        subq $544, %rsp             # allocate 2*216 for Pstrings, 2*4 for integers, 8 for allignment
         movq $format_d, %rdi        # saves format "%d" to rdi - 1st parameter register.
-        leaq -528(%rbp), %rsi       # takes the adress of p1.char
+        leaq -544(%rbp), %rsi       # takes the adress of p1.char
         xor %rax, %rax              # initiallize rax to zero
         call scanf
 
         ## scan 2nd argument p1.str
         movq $format_s, %rdi        # saves format "%s" to rdi.
-        leaq -527(%rbp), %rsi       # takes adrerss of p1.str and puts it in rsi
+        leaq -543(%rbp), %rsi       # takes adrerss of p1.str and puts it in rsi
         xor %rax, %rax
         call scanf
 
         ## scan 3rd argument p2.len
         movq $format_d, %rdi        # saves format "%d" to rdi - 1st parameter register.
-        leaq -272(%rbp), %rsi       # takes the adress of p2.char
+        leaq -279(%rbp), %rsi       # takes the adress of p2.char
         xor %rax, %rax
         call scanf
 
         ## scan 4th argument p2.str
         movq $format_s, %rdi
-        leaq -271(%rbp), %rsi
+        leaq -278(%rbp), %rsi
         xor %rax, %rax
         call scanf
 
         ## scan 5th argument opt
         movq $format_d, %rdi
-        leaq -16(%rbp), %rsi
+        leaq -22(%rbp), %rsi
         xor %rax, %rax
         call scanf
         ##jmp test1
 
         # move &opt to rdi, &p1 to rsi, &p2 to rdx
-        movzbl -16(%rbp), %edi      # takes the char 'opt' and puts it in edi with zero-fill for long
-        leaq -528(%rbp), %rsi
-        leaq -272(%rbp), %rcx
+        movzbl -22(%rbp), %edi      # takes the char 'opt' and puts it in edi with zero-fill for long
+        leaq -544(%rbp), %rsi
+        leaq -279(%rbp), %rcx
         call func_select
 
     end:
